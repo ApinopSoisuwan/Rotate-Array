67d06b451a3ffdd212f6e26d4946865806bf37d2
Apinop <apinop.soisuwan@gmail.com>
2021-11-25 00:25:18 +0700

    using deque

--- Rotate Array.py
+++ Rotate Array.py
@@ -1,5 +1,12 @@
 def rotate(nums,k):
-    return nums[-k:] + nums[:-k]
+    from collections import deque
+    nums = deque(nums)
+    nums.rotate(k)
+    return list(nums)
+
+
+
+
 
 a1 = [1,2,3,4,5,6,7]
 a2 = 3
